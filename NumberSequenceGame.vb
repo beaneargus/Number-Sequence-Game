f4fc0f40c850dd01cabbd352adb34566ca3a6130
Argus Beane <beaneargus@gmail.com>
2015-01-19 15:04:54 -0600

    Made room for prompted mode

--- NumberSequenceGame.vb
+++ NumberSequenceGame.vb
@@ -3,13 +3,25 @@
     Sub Main()
         ' Random Number Generator thing
         Dim r As Random = New Random
-
-        ' Set Variables
-        Dim StartingNumber As Integer = r.Next(1, 10)
-        Dim StepNumber As Integer = r.Next(1, 25)
-
-        Dim CurrentValue As Double = StartingNumber
         Dim Answer As Double = 0
+        Dim CurrentValue As Double
+        Dim StepNumber As Integer
+
+        ' Cube or Sphere
+        Console.WriteLine("Random Number Game: input 1 for game mode,input 2 for prompted mode")
+        input = Console.ReadLine()
+
+        If input = "1" Then
+            ' Set Variables
+            Dim StartingNumber As Integer = r.Next(1, 10)
+            StepNumber = r.Next(1, 25)
+            CurrentValue = StartingNumber
+        ElseIf input = "2" Then
+            'Add prompted mode here
+            Console.WriteLine("coming soon")
+        Else
+            Console.WriteLine("You did not enter a 1 or 2, the game will exit.")
+        End If
 
         ' Instructions
         Console.WriteLine("Read sequence of numbers and guess the next number:")
